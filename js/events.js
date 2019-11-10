34a60cde8a7c6d9bc01ea79f8adc5c84a6c84244
Bloal <fazt@faztweb.com>
2019-11-09 19:11:38 -0600

    quinto intento

--- js/events.js
+++ js/events.js
@@ -1,20 +1,33 @@
+$(".contenedor5").css("display","none");
+$(".contenedor6").css("display","none");
 
 $("#services").click(function(){
 $(".contenedor2").hide();
 $(".contenedor4").hide();
+$(".contenedor3").hide();
 $(".contenedor3").show();
+
 
 });
 
 $("#index").click(function(){
+$(".contenedor5").hide();
 $(".contenedor2").show();	
 $(".contenedor3").show();
 $(".contenedor4").show();
 });
 
 $("#contactos").click(function(){
+$(".contenedor5").hide();
 $(".contenedor2").hide();	
 $(".contenedor3").hide();
 $(".contenedor4").show();
-})
+});
 
+
+$("#portafolio").click(function(){
+$(".contenedor2").hide();	
+$(".contenedor3").hide();
+$(".contenedor4").hide();
+$(".contenedor5").show();
+});
