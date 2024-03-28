7f6fbaf61c432251a05ac3d8ccf72e3a90c7c5d5
Liz <joanaReyna4@gmail.com>
2024-03-27 19:01:37 -0600

    Add files via upload

---
+++ contenedores.php
@@ -0,0 +1,158 @@
+<header class="contenedor2">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 col-sm-12 border1 py-5 px-5">
+				<div class="col-lg-11 border2">
+					<p>B'LOAL</p>
+					<div class="row">
+						<div class="col-lg-4">
+							<h3>Siempre Bella</h3>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
+<section class="contenedor3">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-lg-12 my-4">
+				<div class="col-lg-12 col-sm-12 prueba">
+					<div class="row justify-content-center">
+						<div class="col-lg-3 my-2 titles" >
+							<div class="subtitles">
+								<p class="text-center">Limpieza Facial</p>
+								<span class="row justify-content-center imagen">
+									<img src="imgs/crema.png"></span>
+							</div>
+			
+						</div>
+						<div class="col-lg-3 my-2 titles" >
+							<div class="subtitles">
+								<p class="text-center">Aplicación de Uñas</p>
+								<span class="row justify-content-center imagen">
+									<img src="imgs/unas.png"></span>
+								</div>
+						</div>
+						<div class="col-lg-3 my-2 titles" >
+							<div class="subtitles">
+								<p class="text-center">Aplicación de Tintes</p>
+								<span class="row justify-content-center imagen">
+									<img src="imgs/champu.png"></span>
+								</div>
+						</div>
+						<div class="col-lg-3 my-2 titles" >
+							<div class="subtitles">
+								<p class="text-center">Aplicación de Pestañas</p>
+								<span class="row justify-content-center imagen">
+									<img src="imgs/rizador-de-pestanas.png"></span>
+								</div>
+						</div>
+						<div class="col-lg-3 my-2 titles" >
+							<div class="subtitles">
+								<p class="text-center">Corte de cabello</p>
+								<span class="row justify-content-center imagen">
+									<img src="imgs/peine.png"></span>
+								</div>
+						</div>
+						<div class="col-lg-3 my-2 titles" >
+							<div class="subtitles">
+								<p class="text-center">Peinados</p>
+								<span class="row justify-content-center imagen">
+									<img src="imgs/plancha.png"></span>
+								</div>
+						</div>
+						<div class="col-lg-3 my-2 titles" >
+							<div class="subtitles">
+								<p class="text-center">Maquillaje</p>
+								<span class="row justify-content-center imagen">
+									<img src="imgs/cepillo.png"></span>
+								</div>
+						</div>
+						
+				</div>
+		</div>
+	</div>
+</div>
+<div class="row justify-content-center">
+	<div class="col-lg-12">
+		<div class="row justify-content-center">
+		<div class="col-lg-6 tablBorder1">
+			<table class="table table-condensed table-hover table-dark">
+    <thead>
+      <tr>
+        <th>Servicios</th>
+        <th>Precios</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Limpieza Facial</td>
+        <td>150</td>
+      </tr>
+      <tr>
+        <td>Aplicación de Uñas</td>
+        <td>250</td>
+      </tr>
+      <tr>
+        <td>Aplicación de Tintes</td>
+        <td>230</td>
+      </tr>
+      <tr>
+        <td>Aplicación de Pestañas</td>
+        <td>250</td>
+      </tr>
+       <tr>
+        <td>Corte de Caballo</td>
+        <td>150</td>
+      </tr>
+       <tr>
+        <td>Peinados</td>
+        <td>250</td>
+      </tr>
+       <tr>
+        <td>Maquillaje</td>
+        <td>450</td>
+      </tr>
+    </tbody>
+  </table>
+		</div>
+	</div>
+</div>
+</div>
+</div>
+</section>
+
+<section class="contenedor4">	
+<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-lg-12 py-2 fondo">
+				<div class="row justify-content-center">
+					<div class="col-lg-10">
+						<h6 class="text-center contact">Contactos</h6>
+
+					 	<div class="row justify-content-center">
+					 		<table>
+					 			<tbody class="tbodyInfo">
+					 			<tr>
+					 				<td><p id="ImportDate">Numero:</p></td>
+					 				<td><p id="date">81 2101 88 71<p></td>
+					 			</tr>
+					 			<tr>
+					 				<td><p id="ImportDate">Ubicación:</p></td>
+					 			    <td><p id="date">Venustiano Carranza 229,Constituyentes de Queretaro 3er Sector, San Nicolás de los Garza N.L.</p></td>
+					 			</tr>
+
+					 			</tbody>
+					 		</table>
+                        	<iframe class="mapa" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d731.3024530918563!2d-100.25436017125821!3d25.721134907203666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8662ead6a514f007%3A0x89df6f027c45c9db!2sVenustiano%20Carranza%20229%2C%20Constituyentes%20de%20Queretaro%203er%20Sector%2C%2066490%20San%20Nicol%C3%A1s%20de%20los%20Garza%2C%20N.L.!5e1!3m2!1ses!2smx!4v1573257175919!5m2!1ses!2smx" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="">
+                        	
+                        </iframe>
+					    </div>
+					</div>
+			</div>
+		</div>
+	</div>
+</div>
+</section>
